527a471dc9a6317f35cbeeaa7eef42b7cf314ebe
joshua-8 <joshua_phelps@brown.edu>
2024-08-14 01:30:06 -0700

    add indicator lights

--- src/controller.ino
+++ src/controller.ino
@@ -2,12 +2,12 @@
 
 #include "OneAnalogJoystick.h"
 
-OneAnalogJoystick stick = OneAnalogJoystick(A0, D5, D0, D7, D6);
+OneAnalogJoystick stick = OneAnalogJoystick(A0, D7, D6, D5, D0);
 
 float x = 0;
 float rz = 0;
 
-float batteryVoltage = 0;
+float batteryVoltage = 5;
 
 // data receive callback
 void WifiDataToParse()
@@ -30,6 +30,8 @@ void configWifi()
     EWD::routerPassword = "password001";
     EWD::routerPort = 25001;
     EWD::communicateWithIP = "192.168.4.1";
+    EWD::signalLossTimeout = 200;
+    EWD::resendTimeout = 50;
 }
 
 void setup()
@@ -37,15 +39,40 @@ void setup()
     Serial.begin(115200);
     configWifi();
     EWD::setupWifi(WifiDataToParse, WifiDataToSend);
+
+    pinMode(D3, OUTPUT); // blue (connected)
+    pinMode(D1, OUTPUT); // middle red (not connected)
+    pinMode(D2, OUTPUT); // bottom red (robot battery low)
 }
 
 void loop()
 {
-    x = constrain((stick.readY()-525)/450.0,-1,1);
-    rz = constrain((stick.readX()-573)/450.0,-1,1);
+    x = constrain((stick.readY() - 475) / 425.0, -1, 1);
+    rz = constrain((stick.readX() - 601) / 400.0, -1, 1);
+
+    // print x, rz to serial
+    // Serial.print(x);
+    // Serial.print(" ");
+    // Serial.println(rz);
+
     EWD::runWifiCommunication();
-    if (EWD::newData()) {
-        Serial.println(batteryVoltage);
+    // FOR LEDS LOW IS ON
+    if (EWD::getWifiConnected()) {
+        digitalWrite(D3, LOW);
+        digitalWrite(D1, HIGH);
+    } else {
+        digitalWrite(D3, HIGH);
+        digitalWrite(D1, LOW);
     }
+
+    if (batteryVoltage >= 3.55) {
+        digitalWrite(D2, HIGH);
+    } else { // battery low
+        digitalWrite(D2, LOW);
+    }
+
+    // if (EWD::newData()) {
+    //     Serial.println(batteryVoltage);
+    // }
     delay(10); // shorter delays bork the wifi
 }
